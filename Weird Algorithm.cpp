f1e59a2faa9863365ad5768b270c09104dac833b
darth_coder <33444220+saket0565@users.noreply.github.com>
2020-12-28 14:11:48 +0530

    Create Weird Algorithm.cpp

---
+++ Weird Algorithm.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#define ll long long int
+int main() {
+	// your code goes here
+	ll n;
+	cin>>n;
+    cout<<n<<" ";
+	while(n!=1){
+	    if(n&1)
+	        n=3*n+1;
+	    else
+	        n/=2;
+        cout<<n<<" ";
+	}
+	return 0;
+}
